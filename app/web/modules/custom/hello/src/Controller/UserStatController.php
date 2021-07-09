d32dcd160239d5d8fa4ab2b05fc6e2cbfc61f808
Jean BOITTIAUX <jean.boittiaux@galian.fr>
2021-07-09 16:33:37 +0200

    Ajout des donnees de connexion sur la page statistiques

--- app/web/modules/custom/hello/src/Controller/UserStatController.php
+++ app/web/modules/custom/hello/src/Controller/UserStatController.php
@@ -8,10 +8,29 @@ use Drupal\user\UserInterface;
 class UserStatController extends ControllerBase {
 
     public function content(UserInterface $user) {
+        $results = \Drupal::database()
+            ->select('hello_user_statistics', 's')
+            ->fields('s', ['time', 'action'])
+            ->condition('uid', $user->id(), '=')
+            ->orderBy('time', 'DESC')
+            ->execute()
+        ;
 
+        $rows = [];
+        foreach ($results as $row) {
+            $rows[] = [
+                'action' => $row->action ? 'Login' : 'Logout',
+                'time' => \Drupal::service('date.formatter')->format($row->time),
+            ];
+        }
 
         return [
-            '#markup' => 'test',
+            '#type' => 'table',
+            '#rows' => $rows,
+            '#header' => [
+                $this->t('Action'),
+                $this->t('Date'),
+            ],
         ];
     }
 }
