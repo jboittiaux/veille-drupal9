bf4b342818e11c6720f99259bf5712fcf15df3bf
Jean BOITTIAUX <jean.boittiaux@galian.fr>
2021-07-09 14:14:21 +0200

    Ajout de la liste des nodes sur la page hello list

--- app/web/modules/custom/hello/src/Controller/HelloListController.php
+++ app/web/modules/custom/hello/src/Controller/HelloListController.php
@@ -5,9 +5,19 @@ namespace Drupal\hello\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
 class HelloListController extends ControllerBase {
+
     public function content() {
+        $nodeStorage = \Drupal::entityTypeManager()->getStorage('node');
+        $nodes = $nodeStorage->loadMultiple();
+
+        $items = [];
+        foreach ($nodes as $node) {
+            $items[] = $node->label();
+        }
+
         return [
-            '#markup' => $this->t('Hello List'),
+            '#theme' => 'item_list',
+            '#items' => $items,
         ];
     }
 }
